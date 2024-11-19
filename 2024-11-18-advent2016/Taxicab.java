4ea8a513e1b5cd37828ecec6c8de148cf754c7c9
Leilah Yik <leilahy@nycstudents.net>
2024-11-18 23:14:14 -0500

    attempted and failed directionCount

--- 2024-11-18-advent2016/Taxicab.java
+++ 2024-11-18-advent2016/Taxicab.java
@@ -3,6 +3,10 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Taxicab {
+  public static void main(String[] args){
+    System.out.println(directionCount("input.txt"));
+  }
+
   public static int directionCount(String filename){
     try {
       File file = new File(filename);//1
@@ -16,18 +20,33 @@ public class Taxicab {
       int facing = 0;
 
       for (int i = 0; i < inp.length; i++){
+        // dealing w direction
         if (inp[i].substring(0,1).equals("R")){
           facing = (facing + 1) % 4;
         }
         else facing = (facing + 3) % 4;
+        // finding direction
+        if (facing == 0){
+          y += Integer.parseInt(inp[i].substring(1));
+        }
+        else if (facing == 1){
+          x += Integer.parseInt(inp[i].substring(1));
+        }
+        else if (facing == 2){
+          y -= Integer.parseInt(inp[i].substring(1));
+        }
+        else if (facing == 3){
+          x -= Integer.parseInt(inp[i].substring(1));
+        }
       }
 
-
-      //input.close();//releases the file from your program
+      input.close();
+      return x + y;
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println(filename);
+      return -1;
   }
 }
 }
