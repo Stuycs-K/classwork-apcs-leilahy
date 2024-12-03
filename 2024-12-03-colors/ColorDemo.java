11be24693601323d3ffad22caf9b835e13b61d0f
Leilah Yik <leilahy@nycstudents.net>
2024-12-03 11:59:12 -0500

    added methods

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,4 +1,20 @@
 public class ColorDemo{
+  public static final String CLEAR_SCREEN =  "\u001b[2J";
+  public static final String HIDE_CURSOR =  "\u001b[?25l";
+  public static final String SHOW_CURSOR =  "\u001b[?25h";
+  public static int BLACK = 30;
+  public static int RED = 31;
+
+  public static void color(int foreground,int background){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  }
+
+  public static void color(int foreground, int background, int modifier){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) +  ";" + modifier + "m");
+  }
+  public static void go(int r,int c){
+    System.out.print("\u001b[" + r + ";" + c + "f");
+  }
   public static void main(String[] args){
     // changing modes test
     System.out.print("\u001b[31m");
@@ -27,6 +43,7 @@ public class ColorDemo{
       }
       System.out.println();
     }
+
     }
 
 }
