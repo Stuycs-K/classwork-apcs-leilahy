726a1129b29348366b3f8740b08f5cef782ac7ba
Leilah Yik <leilahy@nycstudents.net>
2024-12-03 11:50:56 -0500

    successful changing color modes

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,10 @@
+public class ColorDemo{
+  public static void main(String[] args){
+    // changing modes test
+    System.out.print("\u001b[31m");
+    System.out.println("Hello World");
+    System.out.print("\u001b[0m");
+    System.out.println("reset");
+  }
+
+}
