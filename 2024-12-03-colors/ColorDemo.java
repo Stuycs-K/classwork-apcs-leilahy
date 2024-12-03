0273a4cb63f9ee05ceedbb7d1c491bbce369b5e7
Leilah Yik <leilahy@nycstudents.net>
2024-12-03 11:53:51 -0500

    tested changing colors of certain words only

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -5,6 +5,19 @@ public class ColorDemo{
     System.out.println("Hello World");
     System.out.print("\u001b[0m");
     System.out.println("reset");
+
+    // red fish and other words
+    System.out.print("\u001b[31mRed");
+    /*
+    System.out.print(" fish ");
+    System.out.print("\u001b[34mBlue");
+    System.out.print(" fish ");
+    System.out.print(" also blue because no reset... also your terminal is blue now too...");
+    */
+    System.out.print("\u001b[31mRed");
+    System.out.print(" \u001b[0mfish ");
+    System.out.print("\u001b[34mBlue");
+    System.out.print(" \u001b[0mfish ");//reset to defaults!
   }
 
 }
