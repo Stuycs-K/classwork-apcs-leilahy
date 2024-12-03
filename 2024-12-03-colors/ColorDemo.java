e4a6c312c4eb53749852a8b7d85ec20d4913011f
Leilah Yik <leilahy@nycstudents.net>
2024-12-03 11:55:12 -0500

    tested printing many RGB colors

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -18,6 +18,15 @@ public class ColorDemo{
     System.out.print(" \u001b[0mfish ");
     System.out.print("\u001b[34mBlue");
     System.out.print(" \u001b[0mfish ");//reset to defaults!
-  }
+    // testing many colors
+    for(int r = 0; r < 256; r+=32){
+      for(int g = 0; g <= 256; g+=32){
+        for(int b = 0; b <= 256; b+=32){
+          System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+        }
+      }
+      System.out.println();
+    }
+    }
 
 }
