fac9d33f98765bdaed4b5b018779f201dad175b1
Leilah Yik <leilahy@nycstudents.net>
2024-12-06 11:29:19 -0500

    improted class files and overwrite abstract methods in warrior calss

---
+++ 2024-12-06-Adventurer/Warrior.java
@@ -0,0 +1,52 @@
+public class Warrior extends Adventurer {
+  private int rages;
+
+  public Warrior(String name){
+    super(name);
+    rages = 5;
+  }
+  public Warrior(String name, int hp){
+    super(name, hp);
+    rages = 5;
+  }
+
+  public String getSpecialName(){
+    return "rage";
+  }
+
+  public int getSpecial(){
+    return rages;
+  }
+
+  public void setSpecial(int n){
+    rages = n;
+  }
+
+  public int getSpecialMax(){
+    return 10;
+  }
+
+  public String attack(Adventurer other){
+    int damage = (int)(Math.random() * 20 + 5);
+    other.applyDamage(damage);
+    return this.getName() + " stabs " + other.getName() + " for " + damage + "hp.";
+  }
+
+  public String support(Adventurer other){
+    int healed = - (int)(Math.random() * 5);
+    other.applyDamage(healed);
+    return this.getName() + " gives a battle cry for " + other.getName() + " restoring " + healed + "hp.";
+  }
+
+  public String support(){
+    int healed = - (int)(Math.random() * 5);
+    this.applyDamage(healed);
+    return this.getName() + " gets into battle position! Restores " + healed + "hp.";
+  }
+
+  public String specialAttack(Adventurer other){
+    int damage = (int)(Math.random() * 3);
+    other.applyDamage(damage);
+    return this.getName() + " glares at " + other.getName() + " for " + damage + "hp.";
+  }
+}
