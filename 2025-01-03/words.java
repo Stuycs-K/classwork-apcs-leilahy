67594b27b902451097d4220977dc56f7bb54cd2c
Leilah Yik <leilahy@nycstudents.net>
2025-01-03 11:47:25 -0500

    started work on toWords, finished makeWords

---
+++ 2025-01-03/words.java
@@ -0,0 +1,18 @@
+public class words {
+  public static void main(String[]args){
+    makeWords(2,"","abcde");
+  }
+
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if (remainingLetters == 0){
+      System.out.println(result);
+    }
+    else for (int i = 0; i < alphabet.length(); i++){
+      makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
+    }
+  }
+
+  public static String toWords(int n){
+    if (n == 0) return "zero";
+  }
+}
